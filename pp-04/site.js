f6b55ee7f87a8eb83ff112d258bf2f7ce1e01dfc
Jiayu Huang <jhuang60@hawk.iit.edu>
2017-10-30 13:16:19 -0500

    jslinter added and done with question 3

--- pp-04/site.js
+++ pp-04/site.js
@@ -37,13 +37,14 @@ console.log('The value of x is:', x, 'It should be 5.');
 
 function arrayEach(array, func) {
   for (i = 0; i < array.length; i++) {
+    var i;
     func(array[i]);
   }
 }
 
 arrayEach(['red','green','blue'], console.log);
 
-console.log(i) // should be 'undefined', not 3
+console.log(typeof i) // should be 'undefined', not 3
 
 //  4. Explain why this function does not modify the global
 //  variable x declared on line 5 above. Write your explanation
